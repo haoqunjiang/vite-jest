ca32557345846007480d689f55b989cccf555bcc
Haoqun Jiang <haoqunjiang@gmail.com>
2021-06-16 16:15:12 +0800

    chore: optimizeDeps is not necessary here

--- examples/vue-app-type-module/vite.config.js
+++ examples/vue-app-type-module/vite.config.js
@@ -4,10 +4,5 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  optimizeDeps: {
-    include: [
-      '@vue/test-utils'
-    ]
-  }
+  plugins: [vue(), vueJsx()]
 })
