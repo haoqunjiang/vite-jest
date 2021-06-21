29d002854deb94ed451afe97d4c3aa2330031fc8
Haoqun Jiang <haoqunjiang@gmail.com>
2021-06-21 16:07:57 +0800

    fix: optimizeDeps is no longer required since @vue/test-utils rc7

--- packages/vite-jest/vite-server.js
+++ packages/vite-jest/vite-server.js
@@ -7,11 +7,6 @@ const viteServer = await createServer({
   server: {
     middlewareMode: true,
   },
-  optimizeDeps: {
-    // esm-bundler.js is inferred as CommonJS by Jest
-    // so we have to optimize it so that we require it from the `.vite/` directory
-    include: ['@vue/test-utils']
-  }
   // hmr: false
 })
 
