58802d33415286304becc59aae5a32b0a708a352
Haoqun Jiang <haoqunjiang@gmail.com>
2021-06-22 09:28:51 +0800

    fix: use mainFields instead of optimizeDeps

--- packages/vite-jest/vite-server.js
+++ packages/vite-jest/vite-server.js
@@ -4,15 +4,21 @@ import { createServer } from 'vite'
 
 const viteServer = await createServer({
   base: '/',
+  resolve: {
+    // `module`, `jsnext:main`, and `jsnext` are not standard Node.js package.json fields.
+    // Entries listed in these fields are not likely recognizable as Node.js ESM.
+    // So we must skip them. Use `exports` or `main` only.
+    mainFields: []
+  },
   server: {
     middlewareMode: true,
   },
   // hmr: false
 })
 
-console.log(viteServer.config.cacheDir)
-// FIXME: use detected rootDir instead of process.cwd()
-const viteCacheDirectory = path.join(process.cwd(), './node_modules/.vite/')
+// Workaround to make Node recognize these files as ES modules
+// FIXME: currently it doesn't work on the first run
+const viteCacheDirectory = viteServer.config.cacheDir
 if (!fs.existsSync(viteCacheDirectory)) {
   fs.mkdirSync(viteCacheDirectory)
 }
