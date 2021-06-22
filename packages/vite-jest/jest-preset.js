3b9266705a32100a03a0ddba6c1f6814a7e7c69c
Haoqun Jiang <haoqunjiang@gmail.com>
2021-06-22 14:03:35 +0800

    feat: support asset files

--- packages/vite-jest/jest-preset.js
+++ packages/vite-jest/jest-preset.js
@@ -1,3 +1,6 @@
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
 const resolveRelative = url => (new URL(url, import.meta.url)).pathname
 
 export default {
@@ -10,6 +13,11 @@ export default {
   transformIgnorePatterns: [
     '!/node_modules/\\.vite/',
   ],
+
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$':
+    require.resolve('jest-transform-stub'),
+  },
 
   reporters: [
     'default',
