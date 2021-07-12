ecda88459b807fc84040ab454d730efb6b358666
Max Beatty <mbeatty@squareup.com>
2021-07-12 14:12:02 -0700

    fix: remove workaround
    fixed in vitejs/vite#4061 and released v2.4.0

--- packages/vite-jest/bin/vite-jest.js
+++ packages/vite-jest/bin/vite-jest.js
@@ -7,10 +7,6 @@ import { createRequire } from 'module'
 
 const require = createRequire(import.meta.url)
 const jestPath = require.resolve('jest/bin/jest')
-const vitePath = require.resolve('vite'); // => vite/dist/node/index.js
-
-const viteClientDirectory = path.resolve(path.dirname(vitePath), '../client');
-fs.writeFileSync(path.join(viteClientDirectory, 'package.json'), JSON.stringify({ type: "module" }))
 
 execa.sync('node', [
   '--experimental-vm-modules',
