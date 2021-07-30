55f389ee88d928dc42f8381bae82245b9a323a4c
James Gosbell <James.Gosbell@tabcorp.com.au>
2021-07-30 11:36:07 +1000

    Fixed react ts jest

--- examples/react-app-ts/jest.config.js
+++ examples/react-app-ts/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testMatch: [
     "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
-    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/*.{spec,test}.{ts,tsx}",
   ],
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
