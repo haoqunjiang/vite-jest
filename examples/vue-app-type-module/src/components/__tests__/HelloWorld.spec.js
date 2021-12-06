536bb33da2b886e0fdf603b2055e7aeb16e0fb8c
Haoqun Jiang <haoqunjiang@gmail.com>
2021-12-06 20:05:37 +0800

    chore: use "Hello vite-jest" for testing

--- examples/vue-app-type-module/src/components/__tests__/HelloWorld.spec.js
+++ examples/vue-app-type-module/src/components/__tests__/HelloWorld.spec.js
@@ -2,6 +2,6 @@ import { mount } from "@vue/test-utils";
 import HelloWorld from "../HelloWorld.vue";
 
 test("hellow world", async () => {
-  const wrapper = mount(HelloWorld, { props: { msg: 'Hello Cypress' } })
-  expect(wrapper.html()).toMatch("Hello Cypress");
+  const wrapper = mount(HelloWorld, { props: { msg: 'Hello vite-jest' } })
+  expect(wrapper.html()).toMatch("Hello vite-jest");
 });
