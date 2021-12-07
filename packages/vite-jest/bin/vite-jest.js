d7f3bdd44fc094d3332bd223e9ccf420cfa0e98a
Haoqun Jiang <haoqunjiang@gmail.com>
2021-12-07 11:19:16 +0800

    fix: should run all tests serially to avoid issues with the caches

--- packages/vite-jest/bin/vite-jest.js
+++ packages/vite-jest/bin/vite-jest.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
 
-import fs from 'fs'
-import path from 'path'
 import execa from 'execa'
 import { createRequire } from 'module'
 
 const require = createRequire(import.meta.url)
 const jestPath = require.resolve('jest/bin/jest')
 
+const additionalArgs = process.argv.slice(2)
+if (!additionalArgs.includes('--runInBand') && !additionalArgs.includes('-i')) {
+  additionalArgs.push('--runInBand')
+}
+
 execa.sync('node', [
   '--experimental-vm-modules',
   jestPath,
-  ...process.argv.slice(2)
+  additionalArgs
 ], {
   stdio: 'inherit'
 })
