d3a96b415ddf84b58fbd3659f218efb47fb6ea8e
Haoqun Jiang <haoqunjiang@gmail.com>
2021-12-07 11:22:17 +0800

    fix: fix @vue/test-utils slots compatibility

--- packages/vite-jest/vite-server.js
+++ packages/vite-jest/vite-server.js
@@ -7,6 +7,14 @@ const viteServer = await createServer({
   server: {
     hmr: false,
     middlewareMode: true,
+  },
+  resolve: {
+    alias: {
+      // For similar reasons as in https://github.com/cypress-io/cypress/blob/570f91dde3a8bd54fd059e1cfe0f85bab8f1a7cb/npm/vite-dev-server/src/startServer.ts#L35-L37
+      vue: 'vue/dist/vue.esm-bundler.js',
+      '@vue/compiler-core': '@vue/compiler-core/dist/compiler-core.cjs.js',
+      '@vue/test-utils': '@vue/test-utils/dist/vue-test-utils.cjs.js',
+    }
   }
 })
 
